a6be9644ba7003d5769fd57cc4d6844fb3386730
Edouard Poitras <epoitras@linuxfoundation.org>
2017-01-24 22:19:12 -0500

    Fixed bug that crashed echo without conversation plugin enabled
    Signed-off-by: Edouard Poitras <epoitras@linuxfoundation.org>

--- echo.py
+++ echo.py
@@ -6,4 +6,6 @@ def interaction(context):
     if not context.response_ready():
         prefix = conf['plugins']['echo']['config']['echo_prefix']
         context.set_output_text('%s%s' %(prefix, context.get_input_text()))
-        context.conversation.close()
+        # Close conversation if plugin is used.
+        try: context.conversation.close()
+        except: pass
